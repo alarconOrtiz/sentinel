b4caee392fc5fb38d84cafc9f06cac763ed3df19
alarcon <miguel.alarcon.ortiz@gmail.com>
2022-04-13 13:51:39 +0200

    selection main network interface by scapy

--- sentinel.py
+++ sentinel.py
@@ -1,11 +1,15 @@
-from scapy.all import Ether, ARP,sendp,srp
+import scapy.all as scapy
+#from scapy  import Ether, ARP, srp
+from NetworkDevicesData import NetworkDevicesData
 
 def main():
-    ARPpkt = Ether(dst='ff:ff:ff:ff:ff')/ARP(op=1,pdst='192.168.0.1/24')
-    received = srp(ARPpkt,timeout=2,iface="en0")
+    interfaces = scapy.get_if_hwaddr(scapy.conf.iface)
+    print(interfaces)
+    devicesInfo : list[NetworkDevicesData] = []
+    ARPpkt = scapy.Ether(dst='ff:ff:ff:ff:ff')/scapy.ARP(op=1,pdst='192.168.0.1/24')
+    received = scapy.srp(ARPpkt,timeout=2,iface=scapy.conf.iface)
     for i in range(len(received[0])):
-        print(received[0][i].answer.hwsrc)
-        print(received[0][i].answer.psrc)
-
+        devicesInfo.append(NetworkDevicesData(mac=received[0][i].answer.hwsrc,
+                                            ip=received[0][i].answer.psrc))
 if __name__ == "__main__":
     main()
